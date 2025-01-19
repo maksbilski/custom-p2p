67b81d5bf163e78c42025d1d1eaa8fcbf8d285f7
maksbilski <maksde25@gmail.com>
2025-01-19 17:44:17 +0100

    IMPLEMENT: Added protocol specification header

---
+++ include/p2p-resource-sync/protocol.hpp
@@ -0,0 +1,17 @@
+/**
+ * @brief Structure representing resource download request
+ *
+ * Used for sending requests over TCP to download resources from remote nodes.
+ * The structure has a variable-length field (resourceName) which must always
+ * be placed at the end of the structure.
+ */
+#pragma pack(1)
+struct ResourceRequest {
+    uint32_t messageLength;
+    uint32_t resourceNameLength;
+    uint64_t offset;
+    char resourceName[];
+};
+#pragma pack()
+
+

4e964e9dd81907edb94c40a526f629de97be7710
mrozek <01178582@pw.edu.pl>
2025-01-19 02:43:54 +0100

    IMPLEMENT: Implemented header for announcement broadcaster class

---
+++ include/p2p-resource-sync/announcement_broadcaster.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "local_resource_manager.hpp"
+#include <chrono>
+#include <cstdint>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <vector>
+
+namespace p2p {
+
+typedef struct {
+  uint32_t nameLength;
+  std::string name;
+  uint32_t resourceSize;
+} Resource;
+
+typedef struct {
+  uint32_t datagramLength;
+  uint64_t timestamp;
+  uint32_t resourceCount;
+  std::vector<Resource> resources;
+} AnnounceMessage;
+
+class AnnouncementBroadcaster {
+public:
+  AnnouncementBroadcaster(
+      LocalResourceManager &resource_manager, uint16_t port,
+      std::chrono::seconds broadcast_interval = std::chrono::seconds(30));
+
+  ~AnnouncementBroadcaster();
+
+  // Delete copy and move operations
+  AnnouncementBroadcaster(const AnnouncementBroadcaster &) = delete;
+  AnnouncementBroadcaster &operator=(const AnnouncementBroadcaster &) = delete;
+
+  void run();
+
+private:
+  AnnounceMessage createAnnounceMessage() const;
+
+  void broadcastAnnouncement() const;
+
+  void initializeSocket();
+
+  LocalResourceManager &resource_manager_;
+  uint16_t port_;
+  std::chrono::seconds broadcast_interval_;
+
+  int socket_;
+  struct sockaddr_in broadcast_address_;
+};
+
+} // namespace p2p

b55d0c294ad533ee5fb68f94dcf4b1e35b5942d0
mrozek <01178582@pw.edu.pl>
2025-01-19 01:56:32 +0100

    REFACTOR: Cleaned up tests a bit by creating methods in test class

--- tests/remote_resource_manager_test.cpp
+++ tests/remote_resource_manager_test.cpp
@@ -5,54 +5,52 @@
 #include <gtest/gtest.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <thread>
 #include <vector>
 
 class RemoteResourceManagerTest : public ::testing::Test {
 protected:
+  RemoteResourceManagerTest() : manager(std::chrono::seconds(2)) {}
+
+  p2p::RemoteResourceManager manager;
+
   void SetUp() override {}
 
   void TearDown() override {}
+
+  struct sockaddr_in createAddress(const char *ip, uint16_t port) {
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    inet_pton(AF_INET, ip, &addr.sin_addr);
+    return addr;
+  }
+
+  void addFreshNode(const sockaddr_in &addr) {
+    std::vector<p2p::RemoteResource> resources = {{"test.txt", 1000}};
+    manager.addOrUpdateNodeResources(addr, resources);
+  }
 };
 
-TEST_F(RemoteResourceManagerTest, CanBeInstantiated) {
-  EXPECT_NO_THROW(
-      { p2p::RemoteResourceManager manager(std::chrono::seconds(60)); });
-}
-
 TEST_F(RemoteResourceManagerTest, CannotBeCopied) {
-  p2p::RemoteResourceManager manager(std::chrono::seconds(60));
-
   EXPECT_FALSE(std::is_copy_constructible_v<p2p::RemoteResourceManager>);
   EXPECT_FALSE(std::is_copy_assignable_v<p2p::RemoteResourceManager>);
 }
 
 TEST_F(RemoteResourceManagerTest, EmptyManagerHasNoResources) {
-  p2p::RemoteResourceManager manager(std::chrono::seconds(60));
-
   EXPECT_TRUE(manager.getAllResources().empty());
 }
 
 TEST_F(RemoteResourceManagerTest, CanAddSingleResourceToNode) {
-  p2p::RemoteResourceManager manager(std::chrono::seconds(60));
-  struct sockaddr_in node_address1;
-  memset(&node_address1, 0, sizeof(node_address1));
-  node_address1.sin_family = AF_INET;
-  node_address1.sin_port = htons(8080);
-  node_address1.sin_addr.s_addr = inet_addr("127.0.0.1");
-  p2p::RemoteResource resource{.name = "test_resource", .size = 100};
+  struct sockaddr_in node_address1 = this->createAddress("192.168.1.1", 8000);
 
-  manager.addOrUpdateNodeResources(node_address1, {resource});
+  this->addFreshNode(node_address1);
 
   EXPECT_FALSE(manager.getAllResources().empty());
 }
 
 TEST_F(RemoteResourceManagerTest, CanAddMultipleResourcesToNode) {
-  p2p::RemoteResourceManager manager(std::chrono::seconds(60));
-  struct sockaddr_in node_address1;
-  memset(&node_address1, 0, sizeof(node_address1));
-  node_address1.sin_family = AF_INET;
-  node_address1.sin_port = htons(8080);
-  node_address1.sin_addr.s_addr = inet_addr("127.0.0.1");
+  struct sockaddr_in node_address1 = this->createAddress("192.168.1.1", 8000);
   p2p::RemoteResource resource1{.name = "test_resource1", .size = 100};
   p2p::RemoteResource resource2{.name = "test_resource2", .size = 100};
 
@@ -62,17 +60,8 @@ TEST_F(RemoteResourceManagerTest, CanAddMultipleResourcesToNode) {
 }
 
 TEST_F(RemoteResourceManagerTest, CanAddMultipleNodesWithResources) {
-  p2p::RemoteResourceManager manager(std::chrono::seconds(60));
-  struct sockaddr_in node_address1;
-  memset(&node_address1, 0, sizeof(node_address1));
-  node_address1.sin_family = AF_INET;
-  node_address1.sin_port = htons(8080);
-  node_address1.sin_addr.s_addr = inet_addr("127.0.0.1");
-  struct sockaddr_in node_address2;
-  memset(&node_address2, 0, sizeof(node_address2));
-  node_address2.sin_family = AF_INET;
-  node_address2.sin_port = htons(8080);
-  node_address2.sin_addr.s_addr = inet_addr("127.0.0.2");
+  struct sockaddr_in node_address1 = this->createAddress("192.168.1.1", 8000);
+  struct sockaddr_in node_address2 = this->createAddress("192.168.1.2", 8000);
   p2p::RemoteResource resource1{.name = "test_resource1", .size = 100};
   p2p::RemoteResource resource2{.name = "test_resource2", .size = 100};
 
@@ -83,92 +72,87 @@ TEST_F(RemoteResourceManagerTest, CanAddMultipleNodesWithResources) {
 }
 
 TEST_F(RemoteResourceManagerTest, NodeHasResource) {
-  p2p::RemoteResourceManager manager(std::chrono::seconds(60));
-  struct sockaddr_in node_address1;
-  memset(&node_address1, 0, sizeof(node_address1));
-  node_address1.sin_family = AF_INET;
-  node_address1.sin_port = htons(8080);
-  node_address1.sin_addr.s_addr = inet_addr("127.0.0.1");
-  p2p::RemoteResource resource{.name = "test_resource", .size = 100};
+  struct sockaddr_in node_address1 = this->createAddress("192.168.1.1", 8000);
 
-  manager.addOrUpdateNodeResources(node_address1, {resource});
+  this->addFreshNode(node_address1);
 
-  EXPECT_TRUE(manager.hasResource(node_address1, "test_resource"));
+  EXPECT_TRUE(manager.hasResource(node_address1, "test.txt"));
 }
 
 TEST_F(RemoteResourceManagerTest, NodeHasNoResource) {
-  p2p::RemoteResourceManager manager(std::chrono::seconds(60));
-  struct sockaddr_in node_address1;
-  memset(&node_address1, 0, sizeof(node_address1));
-  node_address1.sin_family = AF_INET;
-  node_address1.sin_port = htons(8080);
-  node_address1.sin_addr.s_addr = inet_addr("127.0.0.1");
-  p2p::RemoteResource resource{.name = "test_resource", .size = 100};
+  struct sockaddr_in node_address1 = this->createAddress("192.168.1.1", 8000);
 
-  manager.addOrUpdateNodeResources(node_address1, {resource});
+  this->addFreshNode(node_address1);
 
-  EXPECT_FALSE(manager.hasResource(node_address1, "test_resource2"));
+  EXPECT_FALSE(manager.hasResource(node_address1, "test_resource"));
 }
 
 TEST_F(RemoteResourceManagerTest, FindNodeWithResource) {
-  p2p::RemoteResourceManager manager(std::chrono::seconds(60));
-  struct sockaddr_in node_address1;
-  memset(&node_address1, 0, sizeof(node_address1));
-  node_address1.sin_family = AF_INET;
-  node_address1.sin_port = htons(8080);
-  node_address1.sin_addr.s_addr = inet_addr("127.0.0.1");
-  p2p::RemoteResource resource{.name = "test_resource", .size = 100};
-
-  manager.addOrUpdateNodeResources(node_address1, {resource});
+  struct sockaddr_in node_address1 = this->createAddress("192.168.1.1", 8000);
+  this->addFreshNode(node_address1);
 
   std::vector<struct sockaddr_in> found_nodes =
-      manager.findNodesWithResource("test_resource");
+      manager.findNodesWithResource("test.txt");
 
   EXPECT_FALSE(found_nodes.empty());
 }
 
 TEST_F(RemoteResourceManagerTest, FindMultipleNodesWithResource) {
-  p2p::RemoteResourceManager manager(std::chrono::seconds(60));
-  struct sockaddr_in node_address1;
-  memset(&node_address1, 0, sizeof(node_address1));
-  node_address1.sin_family = AF_INET;
-  node_address1.sin_port = htons(8080);
-  node_address1.sin_addr.s_addr = inet_addr("127.0.0.1");
-  struct sockaddr_in node_address2;
-  memset(&node_address2, 0, sizeof(node_address2));
-  node_address2.sin_family = AF_INET;
-  node_address2.sin_port = htons(8080);
-  node_address2.sin_addr.s_addr = inet_addr("127.0.0.2");
-  p2p::RemoteResource resource{.name = "test_resource", .size = 100};
-
-  manager.addOrUpdateNodeResources(node_address1, {resource});
-  manager.addOrUpdateNodeResources(node_address2, {resource});
+  struct sockaddr_in node_address1 = this->createAddress("192.168.1.1", 8000);
+  struct sockaddr_in node_address2 = this->createAddress("192.168.1.2", 8000);
+  this->addFreshNode(node_address1);
+  this->addFreshNode(node_address2);
 
   std::vector<struct sockaddr_in> found_nodes =
-      manager.findNodesWithResource("test_resource");
+      manager.findNodesWithResource("test.txt");
 
   EXPECT_TRUE(found_nodes.size() == 2);
 }
 
 TEST_F(RemoteResourceManagerTest, NoNodesWithResourceFound) {
-  p2p::RemoteResourceManager manager(std::chrono::seconds(60));
-  struct sockaddr_in node_address1;
-  memset(&node_address1, 0, sizeof(node_address1));
-  node_address1.sin_family = AF_INET;
-  node_address1.sin_port = htons(8080);
-  node_address1.sin_addr.s_addr = inet_addr("127.0.0.1");
-  struct sockaddr_in node_address2;
-  memset(&node_address2, 0, sizeof(node_address2));
-  node_address2.sin_family = AF_INET;
-  node_address2.sin_port = htons(8080);
-  node_address2.sin_addr.s_addr = inet_addr("127.0.0.2");
-  p2p::RemoteResource resource{.name = "test_resource", .size = 100};
-
-  manager.addOrUpdateNodeResources(node_address1, {resource});
-  manager.addOrUpdateNodeResources(node_address2, {resource});
+  struct sockaddr_in node_address1 = this->createAddress("192.168.1.1", 8000);
+  struct sockaddr_in node_address2 = this->createAddress("192.168.1.2", 8000);
+  this->addFreshNode(node_address1);
+  this->addFreshNode(node_address2);
 
   std::vector<struct sockaddr_in> found_nodes =
-      manager.findNodesWithResource("test_resource1");
+      manager.findNodesWithResource("test1.txt");
 
   EXPECT_TRUE(found_nodes.empty());
+}
+
+TEST_F(RemoteResourceManagerTest, CleanupStaleNodesTest) {
+  struct sockaddr_in node_address1 = this->createAddress("192.168.1.1", 8000);
+  struct sockaddr_in node_address2 = this->createAddress("192.168.1.2", 8000);
+  std::vector<p2p::RemoteResource> resources1 = {{"test1.txt", 1000}};
+  std::vector<p2p::RemoteResource> resources2 = {{"test2.txt", 2000}};
+  manager.addOrUpdateNodeResources(node_address1, resources1);
+  manager.addOrUpdateNodeResources(node_address2, resources2);
+
+  EXPECT_TRUE(manager.hasResource(node_address1, "test1.txt"));
+  EXPECT_TRUE(manager.hasResource(node_address2, "test2.txt"));
+
+  std::this_thread::sleep_for(std::chrono::seconds(3));
+  manager.cleanupStaleNodes();
+
+  EXPECT_FALSE(manager.hasResource(node_address1, "test1.txt"));
+  EXPECT_FALSE(manager.hasResource(node_address2, "test2.txt"));
+}
+
+TEST_F(RemoteResourceManagerTest, CleanupStaleNodesSkipTest) {
+  struct sockaddr_in node_address1 = this->createAddress("192.168.1.1", 8000);
+  struct sockaddr_in node_address2 = this->createAddress("192.168.1.2", 8000);
+  std::vector<p2p::RemoteResource> resources1 = {{"test1.txt", 1000}};
+  std::vector<p2p::RemoteResource> resources2 = {{"test2.txt", 2000}};
+  manager.addOrUpdateNodeResources(node_address1, resources1);
+  manager.addOrUpdateNodeResources(node_address2, resources2);
+
+  EXPECT_TRUE(manager.hasResource(node_address1, "test1.txt"));
+  EXPECT_TRUE(manager.hasResource(node_address2, "test2.txt"));
+
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  manager.cleanupStaleNodes();
+
+  EXPECT_TRUE(manager.hasResource(node_address1, "test1.txt"));
+  EXPECT_TRUE(manager.hasResource(node_address2, "test2.txt"));
 }

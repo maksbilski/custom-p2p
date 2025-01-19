20b110ee71330fdc24e7020463da62e5aa093d98
mrozek <01178582@pw.edu.pl>
2025-01-19 01:03:16 +0100

    STYLE: Formatted with conform

--- tests/local_resource_manager_test.cpp
+++ tests/local_resource_manager_test.cpp
@@ -1,30 +1,25 @@
-#include <gtest/gtest.h>
 #include "p2p-resource-sync/local_resource_manager.hpp"
+#include <gtest/gtest.h>
 
 class LocalResourceManagerTest : public ::testing::Test {
 protected:
-    void SetUp() override {
-    }
+  void SetUp() override {}
 
-    void TearDown() override {
-    }
+  void TearDown() override {}
 };
 
 TEST_F(LocalResourceManagerTest, CanBeInstantiated) {
-    EXPECT_NO_THROW({
-        p2p::LocalResourceManager manager;
-    });
+  EXPECT_NO_THROW({ p2p::LocalResourceManager manager; });
 }
 
 TEST_F(LocalResourceManagerTest, CannotBeCopied) {
-    p2p::LocalResourceManager manager1;
-    
-    EXPECT_FALSE(std::is_copy_constructible_v<p2p::LocalResourceManager>);
-    EXPECT_FALSE(std::is_copy_assignable_v<p2p::LocalResourceManager>);
+  p2p::LocalResourceManager manager1;
+
+  EXPECT_FALSE(std::is_copy_constructible_v<p2p::LocalResourceManager>);
+  EXPECT_FALSE(std::is_copy_assignable_v<p2p::LocalResourceManager>);
 }
 
 TEST_F(LocalResourceManagerTest, AddResource) {
-    p2p::LocalResourceManager manager1;
-    manager1.addResource(std::string("some_name"), std::string("some_path"));
+  p2p::LocalResourceManager manager1;
+  manager1.addResource(std::string("some_name"), std::string("some_path"));
 }
-

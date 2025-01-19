240898cf098bdbeaac7c988782a4675f713d00ed
mrozek <01178582@pw.edu.pl>
2025-01-19 01:03:37 +0100

    STYLE: Formatted style

--- src/local_resource_manager.cpp
+++ src/local_resource_manager.cpp
@@ -1,8 +1,8 @@
 #include <p2p-resource-sync/local_resource_manager.hpp>
 
-
 namespace p2p {
-    bool LocalResourceManager::addResource(const std::string& name, const std::string& path) {
-        return true;
-    };
-}
+bool LocalResourceManager::addResource(const std::string &name,
+                                       const std::string &path) {
+  return true;
+};
+} // namespace p2p

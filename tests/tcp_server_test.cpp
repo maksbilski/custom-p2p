77bde79980ac2410bd42dd7447b85edff9fe1bc9
mrozek <01178582@pw.edu.pl>
2025-01-20 02:24:50 +0100

    STYLE: Formatted

--- tests/tcp_server_test.cpp
+++ tests/tcp_server_test.cpp
@@ -1,19 +1,17 @@
-#include "p2p-resource-sync/tcp_server.hpp"
 #include "p2p-resource-sync/local_resource_manager.hpp"
+#include "p2p-resource-sync/tcp_server.hpp"
 #include <gtest/gtest.h>
 #include <memory>
 
 class TcpServerTest : public ::testing::Test {
 protected:
-    std::shared_ptr<p2p::LocalResourceManager> resource_manager;
-    
-    void SetUp() override {
-        resource_manager = std::make_shared<p2p::LocalResourceManager>();
-    }
+  std::shared_ptr<p2p::LocalResourceManager> resource_manager;
+
+  void SetUp() override {
+    resource_manager = std::make_shared<p2p::LocalResourceManager>();
+  }
 };
 
 TEST_F(TcpServerTest, CanBeInstantiated) {
-    EXPECT_NO_THROW({
-        p2p::TcpServer server(resource_manager);
-    });
+  EXPECT_NO_THROW({ p2p::TcpServer server(resource_manager); });
 }
